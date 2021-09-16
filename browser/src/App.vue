9eaa08f5995a006ed481f13a1ac1978b204aea89
gma <gma@nbai.io>
2021-09-16 19:45:04 -0400

    Update browser

---
+++ browser/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+    <div id="app">
+        <router-view v-if="isRouterAlive"></router-view>
+    </div>
+</template>
+<script>
+export default {
+  name: 'APP',
+  provide () {
+      return {
+          reload: this.reload
+      }
+  },
+  data() {
+      return{
+          isRouterAlive: true
+      }
+  },
+  methods: {
+      reload () {
+          this.isRouterAlive = false;
+          this.$nextTick(function () {
+              this.isRouterAlive = true;
+          })
+      }
+  },
+  mounted() {
+    /* window.onbeforeunload = function(){
+      sessionStorage.removeItem('addrWeb')
+    }*/
+  }
+}
+</script>
+<style>
+    @import "./assets/css/main.css";
+    @import "./assets/css/iconfont.css";
+</style>
